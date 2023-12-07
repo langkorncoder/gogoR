7815882d4bdeaf1c5c04fa4eb6100713826619ec
langkorncoder <128065162+langkorncoder@users.noreply.github.com>
2023-12-07 13:02:12 +0100

    Version 0.2.1; Added updated function read_and_clean_data2

--- R/read_and_clean_data2.R
+++ R/read_and_clean_data2.R
@@ -2,7 +2,7 @@
 #' Read and clean data from a CSV file
 #'
 #' This function reads a CSV file with specified separator and decimal point, cleans the column names using the clean_names() function from the janitor package, and returns the cleaned data as a tibble.
-#' It also removes empty  and constant rows. See ??read_and_clean_data2() as the updated version where less cleaning steps were performed.
+#' It also removes empty rows. See ??read_and_clean_data() as the previous version where more cleaning steps were performed.
 #'
 #' @param file_path A character string specifying the path to the CSV file.
 #' @param seperate A character string specifying the separator used in the CSV file. Default is ";".
@@ -11,26 +11,24 @@
 #'
 #' @return A tibble containing the cleaned data.
 #'
-#' @importFrom janitor clean_names remove_empty remove_constant
+#' @importFrom janitor clean_names remove_empty
 #' @importFrom tibble as_tibble
 #' @importFrom utils read.csv
 #'
 #' @export
-read_and_clean_data <- function(file_path, seperate = ";", decim = ",", tibble = TRUE) {
+read_and_clean_data2 <- function(file_path, seperate = ";", decim = ",", tibble = TRUE) {
   # Read the CSV file with specified separator and decimal point
   data <- utils::read.csv(file_path, sep = seperate, dec = decim)
 
   # Clean the column names using the clean_names() function from the janitor package
   cleaned_data <- data %>%
     janitor::clean_names() %>%
-    janitor::remove_empty() %>%
-    janitor::remove_constant()
+    janitor::remove_empty()
 
   if (tibble) {
     cleaned_data <- tibble::as_tibble(cleaned_data)
   }
 
-  print("Be cautious! If you might have constant rows, that are possibly important for your work, you should think about using read_and_clean_data2(). Check ??read_and_clean_data2 for more informations.")
   # Return the cleaned data
   cleaned_data
 }
